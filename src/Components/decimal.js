ec9bfca7537f515f06909e40a96a8bda0ae02eac
Alexander <alexanderl96@yahoo.com>
2021-04-27 01:33:13 -0400

    dfd

--- src/Components/decimal.js
+++ src/Components/decimal.js
@@ -1,10 +1,24 @@
-const decimal = val => {
- if (val === "0") return val
+import React from "react"
+const decimal = val  => {
+    
+ if (val  === "0") return val 
  let display = ""
  let decim = ""
+ let num = false
  if(val.includes(".")){
      display = val.substring(0, val.indexOf("."))
      decim = val.substring(val.indexOf("."))
+ }else{
+     display= val 
  }
+ 
+ if(parseFloat(val )  < 0){
+    num = true
+    display = display.substring(1)
+ }
+ return num
+ ? "-" + parseFloat(display).toLocaleString()+ decim
+ : parseFloat(display).toLocaleString()+ decim
+
 }
 export default decimal
